51ea238fcdd68d1fd4ea4c44dbf08ac0e6cf5ea5
Ramón Roche <mrpollo@gmail.com>
2013-08-30 16:15:19 -0700

    Autocomplete for address fields Based on the autocomplete html attribute spec The following map of Autocomplete tokens is needed to map our address values to correct Browser values that are commonly accepted in all major browser versions, the autocomplete feature was introduced formally in the HTML5 #spec, but it was adopted beforehand by some vendors, lastpass also is able to recognize this values and autocomplete the fields with data previously entered or profiles saved. For more info please visit the wiki page for the autocomplete spec http://wiki.whatwg.org/wiki/Autocompletetype#Supported_Tokens

--- app/helpers/spree/addresses_helper.rb
+++ app/helpers/spree/addresses_helper.rb
@@ -8,7 +8,7 @@ module Spree::AddressesHelper
         is_required = Spree::Address.required_fields.include?(method) || (method == :phone && form.object.phone_required?)
         separator = is_required ? '<span class="req">*</span><br />' : '<br />'
         form.label(method) + separator.html_safe +
-        form.text_field(method, :class => is_required ? 'required' : nil)
+        form.text_field(method, {:class => (is_required ? 'required' : nil)}.merge(autocomplete_attribute(method)))
       end
     end
   end
@@ -21,14 +21,23 @@ module Spree::AddressesHelper
                             :id, :name,
                             {:include_blank => true},
                             {:class => have_states ? "required" : "hidden",
-                            :disabled => !have_states}) +
+                            :disabled => !have_states}.merge(autocomplete_attribute(:state_id))) +
       form.text_field(:state_name,
-                      :class => !have_states ? "required" : "hidden",
-                      :disabled => have_states)
+                      {:class => !have_states ? "required" : "hidden",
+                      :disabled => have_states}.merge(autocomplete_attribute(:state_id)))
       ].join.gsub('"', "'").gsub("\n", "")
 
     form.label(:state, Spree.t(:state)) + '<span class="req">*</span><br />'.html_safe +
       content_tag(:noscript, form.text_field(:state_name, :class => 'required')) +
       javascript_tag("document.write(\"#{state_elements.html_safe}\");")
+  end
+
+  # Please refer to address_fields initializer for more details
+  # on the implementation and values of autocomplete fields
+  def autocomplete_attribute(name)
+    value = AUTOCOMPLETE[name]
+    if value.present?
+      {:autocomplete => value, "x-autocompletetype" => value}
+    end
   end
 end
